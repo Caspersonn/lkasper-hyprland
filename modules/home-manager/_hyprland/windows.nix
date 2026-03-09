49cc02c2de5aa4abf8d3f930b299da2708457353
Caspersonn <lucakasper8@gmail.com>
2026-03-09 13:03:58 +0100

    Fix more configerrors

--- modules/home-manager/_hyprland/windows.nix
+++ modules/home-manager/_hyprland/windows.nix
@@ -1,30 +1,30 @@
 { config, pkgs, ... }: {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "suppressevent maximize, class:.*"
+      "suppress_event maximize, match:class .*"
 
-      "tile, class:^(chromium)$"
+      "tile on, match:class ^(chromium)$"
 
-      "float, class:^(org.pulseaudio.pavucontrol|blueberry.py|nwg-displays)$"
+      "float on, match:class ^(org.pulseaudio.pavucontrol|blueberry.py|nwg-displays)$"
 
-      "float, class:^(steam)$"
-      "fullscreen, class:^(com.libretro.RetroArch)$"
+      "float on, match:class ^(steam)$"
+      "fullscreen on, match:class ^(com.libretro.RetroArch)$"
 
-      "opacity 0.97 0.9, class:.*"
-      "opacity 1 1, class:^(chromium|google-chrome|google-chrome-unstable)$, title:.*Youtube.*"
-      "opacity 1 0.97, class:^(chromium|google-chrome|google-chrome-unstable)$"
-      "opacity 0.97 0.9, initialClass:^(chrome-.*-Default)$"
-      "opacity 1 1, initialClass:^(chrome-youtube.*-Default)$"
-      "opacity 1 1, class:^(zoom|vlc|org.kde.kdenlive|com.obsproject.Studio)$"
-      "opacity 1 1, class:^(com.libretro.RetroArch|steam)$"
+      "opacity 0.97 0.9, match:class .*"
+      "opacity 1 1, match:class ^(chromium|google-chrome|google-chrome-unstable)$, match:title .*Youtube.*"
+      "opacity 1 0.97, match:class ^(chromium|google-chrome|google-chrome-unstable)$"
+      "opacity 0.97 0.9, match:initial_class ^(chrome-.*-Default)$"
+      "opacity 1 1, match:initial_class ^(chrome-youtube.*-Default)$"
+      "opacity 1 1, match:class ^(zoom|vlc|org.kde.kdenlive|com.obsproject.Studio)$"
+      "opacity 1 1, match:class ^(com.libretro.RetroArch|steam)$"
 
-      "nofocus, class:^$, title:^$, xwayland:1, floating:1, fullscreen:0, pinned:0"
+      "no_focus on, match:class ^$, match:title ^$, match:xwayland 1, match:float 1, match:fullscreen 0, match:pin 0"
 
-      "float, title:(clipse)"
-      "size 622 652, title:(clipse)"
-      "stayfocused, title:(clipse)"
+      "float on, match:title (clipse)"
+      "size 622 652, match:title (clipse)"
+      "stay_focused on, match:title (clipse)"
     ];
 
-    layerrule = [ "blur on, namespace:wofi" ];
+    layerrule = [ "blur on, match:namespace wofi" ];
   };
 }
